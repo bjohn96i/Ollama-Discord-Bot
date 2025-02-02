c17941044b9fa6728290dd7a740a1b50ad531519
Johnathan Alexander <johnathanalexander3@gmail.com>
2025-02-02 18:02:49 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Ollama-Discord-Bot
+
+Discord Bot that uses Ollama as a chatbot.
+
+## Requirements
+
+- Node.js v22.13.1 (LTS)
+- A Running Instance of Ollama, preferably running the DeepSeek-R1 LLM
+
+## Installation
+
+1. Install [Node.js](https://nodejs.org/en)
+2. Install [Ollama](https://ollama.com/)
+3. Pull a model. It is preferable to use DeepSeek-R1
+   `ollama run deepseek-r1:14b`
+4. Start Ollama by running `ollama serve`. You may need to stop the ollama service that runs first.
+5. [Create a Discord bot](https://discord.com/developers/applications)
+   - Under Application » Bot
+     - Enable Message Content Intent
+     - Enable Server Members Intent
+6. Invite the bot to a server
+   - Go to Application » OAuth2 » URL Generator
+   - Enable bot
+   - Enable Send Messages, Read Messages/View Channels, and Read Message History
+   - Under Generated URL, click Copy and paste the URL in your browser
+7. Rename .env.example to .env and edit the .env file
+   - You can get the token from Application » Bot » Token. Please keep this private.
+   - Add the Model: `deepseek-r1:8b`
+   - Ollama URL. This should be the same as long as you didn't change it
+   - Set the channel ID and the Bot ID
+     - In Discord, go to User Settings » Advanced, and enable Developer Mode
+     - Right click on a channel you want to use, and click Copy Channel ID
+8. Install the required dependencies with `npm i`
+9. Start the bot with `npm start`
+10. You can interact with the bot by @mentioning it with your message
---
+++ src/app.js
@@ -0,0 +1,90 @@
+import dotenv from "dotenv";
+import axios from "axios";
+import { Logger, LogLevel } from "meklog";
+import { Client, Events, GatewayIntentBits, Partials } from "discord.js";
+
+dotenv.config();
+
+const production =
+  process.env.NODE_ENV == "prod" || process.env.NODE_ENV == "production";
+const log = new Logger(production, "BOT");
+let chatHistory = [];
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+  allowedMentions: { users: [], roles: [], repliedUser: false },
+  partials: [Partials.Channel],
+});
+
+function appendChatHistory(message) {
+  chatHistory.push(message);
+}
+
+function purgeChatHistory() {
+  log(LogLevel.Warning, "Purging Memory...");
+  log(LogLevel.Warning, chatHistory);
+  chatHistory = [];
+}
+
+async function makeRequest(method, path, data) {
+  let config = {
+    method: method,
+    url: `${process.env.OLLAMA}${path}`,
+    data: data,
+  };
+  return await axios(config)
+    .then(function (response) {
+      appendChatHistory(response.data.message);
+      const reply = response.data.message.content.split(/<\/[^>]*>/);
+      log(LogLevel.Info, `Thoughts: ${reply[0]}`);
+      log(LogLevel.Info, `Response: ${reply[1]}`);
+      return reply[1];
+    })
+    .catch(function (error) {
+      log(LogLevel.Fatal, error);
+    });
+}
+
+function chunkResponse(msg) {
+  return msg.match(/[\s\S]{1,2000}/g);
+}
+
+client.on("ready", async () => {
+  log(LogLevel.Info, `Logged in as ${client.user.tag}!`);
+});
+
+client.on(Events.MessageCreate, async (msg) => {
+  if (msg.content.includes(`<@${process.env.BOTID}>`)) {
+    log(LogLevel.Info, `Message Recieved: ${msg.content}`);
+
+    if (msg.content.includes("/purge")) {
+      purgeChatHistory();
+      msg.reply("Memory Purged.");
+    } else {
+      appendChatHistory({
+        role: "user",
+        content: msg.content,
+      });
+
+      let data = JSON.stringify({
+        model: process.env.MODEL,
+        messages: chatHistory,
+        stream: false,
+      });
+      let response = await makeRequest("post", "/api/chat", data);
+      chunkResponse(response).forEach((chunk) => {
+        msg.reply(chunk);
+      });
+      // msg.reply(response);
+    }
+  }
+});
+
+// this line must be at the very end
+client.login(process.env.TOKEN); // signs the bot in with token
